538000e0d45b98b630d8e6a4c212ebf03fcd2643
Renan <renmms42@gmail.com>
2021-09-20 18:12:58 -0300

    App fetch/remove/useEffect

--- src/App.js
+++ src/App.js
@@ -7,21 +7,50 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
-  const fetchTours = async() => {
-    setLoading(true)
+const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
   }
 
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchTours()
+  }, [])
   if (loading) {
     return (
       <main>
         <Loading />
       </main>
-    );
+    )
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    )
   }
   return (
-
-2
-  );
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
-export default App;
+export default App
